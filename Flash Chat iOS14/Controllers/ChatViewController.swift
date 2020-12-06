3bc2cb44bde9d89794cff7cbade8c76f1ba59ef9
Sandun Rajitha <rajitha1911@gmail.com>
2020-12-06 20:41:18 +0530

    set table view to auto scroll with new messages

--- Flash Chat iOS14/Controllers/ChatViewController.swift
+++ Flash Chat iOS14/Controllers/ChatViewController.swift
@@ -65,15 +65,20 @@ class ChatViewController: UIViewController {
                 print("error getting document \(e)")
             } else {
                 self.messages = []
+               
                 if let snapshot = querySnapshot {
+                    
                     for document in snapshot.documents{
                         let data = document.data()
+                        
                         if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
                             let newMessage = Message(sender: messageSender, body: messageBody)
                             self.messages.append(newMessage)
                             
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
                         }
                     }
@@ -92,6 +97,20 @@ extension ChatViewController: UITableViewDataSource{
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         cell.largeContentTitle = "bla"
         cell.label.text = messages[indexPath.row].body
+        
+        if messages[indexPath.row].sender == Auth.auth().currentUser?.email{
+            //message is from the current user
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        } else {
+            cell.rightImageView.isHidden = true
+            cell.leftImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+        
         return cell
     }
 }
